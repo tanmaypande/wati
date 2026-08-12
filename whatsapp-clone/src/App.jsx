4be854525a1365f4b35e92cdd166a44e1a2d3778
tanmay pandey <tanmaypandey4243@gmail.com>
2026-08-12 10:51:22 +0530

    add vercel comd

--- whatsapp-clone/src/App.jsx
+++ whatsapp-clone/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { BrowserRouter, useLocation } from "react-router-dom";
-import Layout from "./components/layout/Layout";
+import Layout from "./components/Layout/Layout";
 import AppRoutes from "./routes/AppRoutes";
 
 
